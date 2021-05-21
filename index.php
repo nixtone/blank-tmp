88847e090f452477e7c8f7129e075aa38b76b1fb
nixtone <nixtone@nxt.ru>
2021-05-21 17:07:49 +0400

    init flex

---
+++ index.php
@@ -0,0 +1,23 @@
+<!DOCTYPE html>
+<html lang="ru">
+<head>
+<meta charset="UTF-8">
+<meta name="viewport" content="width=device-width, initial-scale=1.0">
+<meta name="description" content="<!-- $DESCRIPTION$ -->">
+<meta name="keywords" content="<!-- $KEYWORDS$ -->">
+<link rel="icon" type="image/png" href="/favicon.png">
+<title><!-- $TITLE$ --></title>
+<link rel="stylesheet" href="/static/custom.css">
+</head>
+<body>
+<div id="wrapper">
+	<header>Header</header>
+	<div class="wide">
+		<div class="container">Content</div>
+	</div>
+</div>
+<footer>Footer</footer>
+<script src="/static/jquery-3.5.1.min.js"></script>
+<script src="/static/custom.js"></script>
+</body>
+</html>
