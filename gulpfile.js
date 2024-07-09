2dec4c78d93e316b42f5b3aa903d4fb12514b192
nixtone <nixtone@nxt.ru>
2024-07-09 20:51:22 +0300

    Выгрузка в корень

--- gulpfile.js
+++ gulpfile.js
@@ -1,79 +1,59 @@
-// Подключения
+/*
+const gulpif = require('gulp-if');
+let isProd = false; // dev by default
+
+.pipe(gulpif(!isProd, sourcemaps.init()))
+.pipe(gulpif(!isProd, sourcemaps.write('.')))
+*/
+
 const {src, dest, watch, parallel, series} = require('gulp'),
 
 	// Пакеты
 	scss = require('gulp-sass')(require('sass')),
-	include = require('gulp-include'),
-	clean = require('gulp-clean'),
-	ttf2woff2 = require('gulp-ttf2woff2'),
+	sourcemaps = require('gulp-sourcemaps'),
+	fileinclude = require('gulp-file-include'),
 	browserSync = require('browser-sync').create(),
 
 	// Пути
 	path = {
 		build: {
-			html: 'build/',
-			js: 'build/static',
-			css: 'build/static',
-			img: 'build/static/images',
-			fonts: 'build/static/fonts'
+			html: './',
+			css: './static/'
 		},
 		src: {
-			html: 'src/*.html',
-			js: 'src/js/*.js',
-			styles: 'src/styles/*.scss',
-			img: 'src/images/**/*.*',
-			fonts: 'src/fonts/*.*'
+			html: './src/html/*.html',
+			styles: './src/styles/*.scss',
 		},
 		watch: {
-			html: 'src/**/*.html',
-			js: 'src/js/**/*.js',
-			styles: 'src/styles/**/*.scss',
-			img: 'src/images/**/*.*',
-			fonts: 'src/fonts/**/*.*'
+			html: './src/html/**/*.html',
+			styles: './src/styles/**/*.scss',
 		},
-		source: 'src',
-		dest: 'build'
-	};
+		// source: 'src',
+		dest: './'
 
+};
 
 // 
 function html() {
 	return src(path.src.html)
-		.pipe(include())
-		.pipe(dest(path.build.html))
-		.pipe(browserSync.stream())
+		.pipe(fileinclude({
+			prefix: '@@',
+      		basepath: '@file'
+		}))
+		.pipe(dest(path.build.html));
 }
 
-// 
-function scripts() {
-	return src(path.src.js)
-		.pipe(dest(path.build.js))
-		.pipe(browserSync.stream())
-}
-
-// 
+//
 function styles() {
 	return src(path.src.styles)
+		.pipe(sourcemaps.init())
 		.pipe(scss())
+		.pipe(sourcemaps.write())
 		.pipe(dest(path.build.css))
-		.pipe(browserSync.stream())
+		.pipe(browserSync.stream());
 }
 
-// Не работает (пустые картинки)
-function images() {
-	return src(path.src.img)
-		.pipe(dest(path.build.img))
-}
-
-// 
-function fonts() {
-	return src(path.src.fonts)
-		.pipe(ttf2woff2())
-		.pipe(dest(path.build.fonts))
-}
-
-
-// 
+//
 function watcher() {
 	browserSync.init({
         server: {
@@ -81,36 +61,17 @@ function watcher() {
         }
     });
     watch(path.watch.html, html)
-    watch(path.watch.js, scripts)
 	watch(path.watch.styles, styles)
-	watch(path.watch.img, images)
-	watch(path.watch.fonts, fonts)
 	.on('change', browserSync.reload);
 }
 
 // 
-function cleanBuild() {
-	return src(path.dest + "/*")
-		.pipe(clean())
-}
 
-// TODO: Доработать (сваливает в кучу весь ассет)
-function builder() {
-	return src(Object.values(path.src)) // ", {base: path.source}"
-		.pipe(dest(path.dest))
-}
 
 
 // Вывод
 exports.html = html;
-exports.scripts = scripts;
 exports.styles = styles;
-exports.images = images;
-exports.fonts = fonts;
-
-exports.cleanBuild = cleanBuild;
 exports.watcher = watcher;
-exports.builder = builder;
 
-exports.build = series(cleanBuild, builder);
-exports.default = parallel(html, scripts, styles, images, fonts, watcher)
+exports.default = parallel(html, styles, watcher);
--- readme.md
+++ readme.md
@@ -12,10 +12,21 @@
 'npm init'  
 'npm i gulp -D'  
 'npm i gulp-sass sass -D' [gulp-sass](https://www.npmjs.com/package/gulp-sass)  
-'npm i gulp-include -D' [gulp-include](https://www.npmjs.com/package/gulp-include)  
-'npm i gulp-clean -D' [gulp-clean](https://www.npmjs.com/package/gulp-clean)  
-'npm i gulp-ttf2woff2 -D' [gulp-ttf2woff2](https://www.npmjs.com/package/gulp-ttf2woff2)  
 'npm i browser-sync -D' [browser-sync](https://www.npmjs.com/package/browser-sync)  
+'npm i gulp-file-include' [gulp-file-include](https://www.npmjs.com/package/gulp-file-include)  
+'npm i gulp-sourcemaps' [gulp-sourcemaps](https://www.npmjs.com/package/gulp-sourcemaps)  
+
+#### Архив  
+
+'npm i gulp-include -D' [gulp-include, слабо есть gulp-file-include](https://www.npmjs.com/package/gulp-include)  
+'npm i gulp-clean -D' [gulp-clean, сомнительно](https://www.npmjs.com/package/gulp-clean)  
+'npm i gulp-ttf2woff2 -D' [gulp-ttf2woff2, ничего не конвертирует](https://www.npmjs.com/package/gulp-ttf2woff2)  
+
+
+## Запуск  
+
+gulp // запуск сборки с последующем вотчером  
+gulp watcher // вотчер
 
 
 ## TODO:  
